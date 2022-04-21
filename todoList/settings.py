73881c669da29f843aaa1113d7f79ca87749ae22
priyanshpsalian <79307288+priyanshpsalian@users.noreply.github.com>
2022-04-21 21:42:20 +0530

    fifth4

--- todoList/settings.py
+++ todoList/settings.py
@@ -120,7 +120,7 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-django_heroku.settings(local())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
