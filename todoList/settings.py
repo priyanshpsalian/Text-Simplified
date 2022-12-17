72c4fd3f465b3c0db09590246d519e7dcccb1aac
Priyansh Salian <79307288+priyanshpsalian@users.noreply.github.com>
2022-12-17 14:37:35 +0530

    done

--- todoList/settings.py
+++ todoList/settings.py
@@ -120,12 +120,9 @@ MEDIA_URL ='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
